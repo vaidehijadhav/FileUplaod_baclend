f8c4469a8256061c07a4571a7b715909a7203170
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-10-27 22:19:22 +0530

    create Database connection

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then( console.log("DB Connection Successful"))
+    .catch( (error) => {
+        console.log("DB Connection Issue");
+        console.error(error);
+        process.exit(1);
+    });
+}
