35c5d4968c55b8dfe970d1cd8cbd57e7bd6136c8
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-11-11 15:13:50 +0530

    add cloudinary route

---
+++ index.js
@@ -0,0 +1,29 @@
+// app create
+const express = require('express');
+const app = express();
+
+// port find krna hai
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+// middleware add krne hai
+app.use(express.json());
+const fileUpload = require('express-fileupload');
+app.use(fileUpload()); 
+
+// db se connect krna hai
+const db = require("./config/database");
+db.connect();
+
+// cloud se connect krna hai
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+// api route mount krna hai
+const Upload = require("./routes/FileUpload");
+app.use('api/v1/upload', Upload);
+
+// activate server
+app.listen(PORT , () => {
+    console.log(`App is running at  ${PORT}`)
+})
